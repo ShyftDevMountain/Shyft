926d00ed60dddd2cb2bfa5d1b52691dfcc8c2a9e
Nohema Call <you@example.com>
2016-10-24 10:57:18 -0600

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const paths = {
+    app: path.resolve(__dirname, 'src')
+};
+
+module.exports = {
+    entry: './src/app.jsx',
+    output: {
+        publicPath: '/public/',
+        filename: 'bundle.js'
+    },
+    devTool: 'eval-source-map',
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?/,
+                include: paths.app,
+                loader: 'babel?cacheDirectory'
+            },
+            {
+                test: /\.css/,
+                include: paths.app,
+                loader: 'style!css'
+            },
+            {
+                test: /\.less/,
+                include: paths.app,
+                loader: 'style!css!less'
+            }
+        ]
+    }
+};
