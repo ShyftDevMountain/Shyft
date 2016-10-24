f5db4fc37db9261ebbe02129ab9196494d4a9aa7
Kenna Martin <kennalmartin@gmail.com>
2016-10-24 13:56:35 -0600

    added home components to app.jsx

---
+++ src/components/Home/FourWaysToRide/FourWaysToRide.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class FourWaysToRide extends React.Component {
+  render () {
+    return (
+      <div>Four Ways To Ride</div>
+    )
+  }
+}
+
+
+export default FourWaysToRide;
---
+++ src/components/Home/MainJumbotron/MainJumbotron.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class MainJumbotron extends React.Component {
+  render () {
+    return (
+      <div>Main Jumbotron</div>
+    )
+  }
+}
+
+
+export default MainJumbotron;
