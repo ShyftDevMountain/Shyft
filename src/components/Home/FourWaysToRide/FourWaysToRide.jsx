24d6cc24e7b2ef46c6623b40c8585fa127e35199
Nohema Call <you@example.com>
2016-10-25 10:58:14 -0600

    html and css for the home page

--- src/components/Home/FourWaysToRide/FourWaysToRide.jsx
+++ src/components/Home/FourWaysToRide/FourWaysToRide.jsx
@@ -1,9 +1,94 @@
 import React from 'react';
+import './FourWaysToRide.css';
 
 class FourWaysToRide extends React.Component {
   render () {
     return (
-      <div>Four Ways To Ride</div>
+      <div className='container-fluid' >
+            <div className='container' >
+                  <div className="row m-b-xl">
+                        <div className="col-xs-12">
+                            <div className="embedded_video full_width pos_r">
+                                <div className="pos_r">
+                                  <img className="full_width" src="https://cdn.lyft.net/brochure/images/home-poster.21ccd2b5.jpg" alt="Smiley face" />
+                                </div>
+                            </div>
+                        </div>
+                  </div>
+
+                  <div className="row m-b-xl">
+                      <div className="col-xs-12">
+                          <section className='content_for_fourWaysToRide' >
+                            <h2 className='text-xs-center' > Four Ways to Ride</h2>
+
+
+
+
+                              <div className="flex align-group-center flex-row-md text-md-left flex-column text-xs-center m-y-l">
+                                      <div className='img'>
+                                      </div>
+                                      <div className='flexer'>
+                                          <h3 className="text-thin m-b-s p-l-m-sm" data-reactid="150">Lyft Line</h3>
+                                          <p className='m-b-0 '>
+                                            Lyft Line is a shared ride. It matches you with others going the same way, so you can split the cost.
+                                            Price is fixed upfront and always less than original Lyft. Available in select U.S. cities.
+                                          </p>
+                                          <p className='m-b-0 '>
+                                             <a>Learn More</a>
+                                          </p>
+                                      </div>
+                              </div>
+
+                              <div className="flex align-group-center flex-row-md text-md-left flex-column text-xs-center m-y-l">
+                                      <div className='img'>
+                                      </div>
+                                      <div className='flexer'>
+                                          <h3 className="text-thin m-b-s p-l-m-sm" data-reactid="150">Lyft Line</h3>
+                                          <p className='m-b-0 '>
+                                            Lyft Line is a shared ride. It matches you with others going the same way, so you can split the cost.
+                                            Price is fixed upfront and always less than original Lyft. Available in select U.S. cities.
+                                          </p>
+                                          <p className='m-b-0 '>
+                                             <a>Learn More</a>
+                                          </p>
+                                      </div>
+                              </div>
+
+                              <div className="flex align-group-center flex-row-md text-md-left flex-column text-xs-center m-y-l">
+                                      <div className='img'>
+                                      </div>
+                                      <div className='flexer'>
+                                          <h3 className="text-thin m-b-s p-l-m-sm" data-reactid="150">Lyft Line</h3>
+                                          <p className='m-b-0 '>
+                                            Lyft Line is a shared ride. It matches you with others going the same way, so you can split the cost.
+                                            Price is fixed upfront and always less than original Lyft. Available in select U.S. cities.
+                                          </p>
+                                          <p className='m-b-0 '>
+                                             <a>Learn More</a>
+                                          </p>
+                                      </div>
+                              </div>
+
+                              <div className="flex align-group-center flex-row-md text-md-left flex-column text-xs-center m-y-l">
+                                      <div className='img'>
+                                      </div>
+                                      <div className='flexer'>
+                                          <h3 className="text-thin m-b-s p-l-m-sm" data-reactid="150">Lyft Line</h3>
+                                          <p className='m-b-0 '>
+                                            Lyft Line is a shared ride. It matches you with others going the same way, so you can split the cost.
+                                            Price is fixed upfront and always less than original Lyft. Available in select U.S. cities.
+                                          </p>
+                                          <p className='m-b-0 '>
+                                             <a>Learn More</a>
+                                          </p>
+                                      </div>
+                              </div>
+                          </section>
+                      </div>
+                  </div>
+            </div>
+      </div>
+
     )
   }
 }
