075a1405a1b6fe546e8c0c4694dd351f82b5600a
ajwehunt <adamjwehunt@gmail.com>
2016-10-25 10:38:33 -0600

    fixed typos, changed img srcs

--- src/components/Home/RidesInMinutes/RidesInMinutes.jsx
+++ src/components/Home/RidesInMinutes/RidesInMinutes.jsx
@@ -9,7 +9,7 @@ class RidesInMinutes extends React.Component {
         <section className="row intro">
           <div className='col-xs-12 text-center'>
             <h4>Rides In Minutes</h4>
-            <p>Wherever you’re headed, count on Lyft for rides in minutes. The Lyft app matches you with local drivers at the tap of a button. Just request and go.</p>
+            <p>Wherever you’re headed, count on Shyft for rides in minutes. The Shyft app matches you with local drivers at the tap of a button. Just request and go.</p>
           </div>
         </section>
         <section className="row works">
@@ -19,23 +19,23 @@ class RidesInMinutes extends React.Component {
               <div className="text-center col-md-4 col-xs-12 steps">
                 <div>
                   <h3>1. Request</h3>
-                  <p>You’ve got options. Request Lyft, Lyft Plus, Lyft Premier, or Lyft Line.</p>
+                  <p>You’ve got options. Request Shyft, Shyft Plus, Shyft Premier, or Shyft Line.</p>
                 </div>
-                <img className="img-responsive align-center" src="https://cdn.lyft.net/brochure/images/app-screen-request-ride.36f8573c.png" alt="Request"/>
+                <img className="img-responsive align-center" src="src/img/home/app-screen-request-ride.36f8573c.png" alt="Request"/>
               </div>
               <div className="text-center col-md-4 col-xs-12 steps">
                 <div>
                   <h3>2. Ride</h3>
                   <p>Get picked up by the best. Our reliable drivers will get you where you need to go.</p>
                 </div>
-                <img className="img-responsive align-center" src="https://cdn.lyft.net/brochure/images/app-screen-get-picked-up.c8b2e18d.png" alt="Ride"/>
+                <img className="img-responsive align-center" src="src/img/home/app-screen-get-picked-up.c8b2e18d.png" alt="Ride"/>
               </div>
               <div className="text-center col-md-4 col-xs-12 steps">
                 <div>
                 <h3>3. Pay</h3>
                 <p>When the ride ends, just pay and rate your driver through your phone.</p>
                 </div>
-                <img className="img-responsive align-center" src="https://cdn.lyft.net/brochure/images/app-screen-get-there.72b451d1.png" alt="pay" />
+                <img className="img-responsive align-center" src="src/img/home/app-screen-get-there.72b451d1.png" alt="pay" />
               </div>
             </div>
           </div>
