8ecac4d2bcadc0bb5d5a81a857bb49ca25aff95d
elliot richardson <ear78@aol.com>
2016-10-27 17:56:22 -0600

    worked on cities page and input box to get city locations, adam and I are sharing some global code for the input box text styling

--- src/components/Cities/CitiesComp/CitiesComp.jsx
+++ src/components/Cities/CitiesComp/CitiesComp.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+
 import './CitiesComp.css';
+
+import CitiesJumbotron from '../CitiesJumbotron/CitiesJumbotron.jsx';
 
 class CitiesComp extends React.Component{
     render(){
         return (
             <div>
-                This is cities comp!
+                <CitiesJumbotron/>
             </div>
         );
     }
