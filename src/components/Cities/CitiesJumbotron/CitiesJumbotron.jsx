c7f7405b27901aa71b51e4575a91baca59421a90
ajwehunt <adamjwehunt@gmail.com>
2016-10-30 10:33:39 -0600

    cities jumbotron city finder

--- src/components/Cities/CitiesJumbotron/CitiesJumbotron.jsx
+++ src/components/Cities/CitiesJumbotron/CitiesJumbotron.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import Geosuggest from 'react-geosuggest-plus';
+import {getZip} from '../../../services/citiesListService.js';
 
 import './CitiesJumbotron.css';
 
 class CitiesJumbotron extends React.Component{
+
     handleInput(){
     }
+
+    onSuggestSelectCity (city) {
+      var lat = city.location.lat;
+      var lng = city.location.lng;
+      console.log(getZip(lat, lng))
+    }
+
     render(){
         return (
             <div>
@@ -13,9 +22,11 @@ class CitiesJumbotron extends React.Component{
                     <div className="cities-jumbotron-title">Cities We're In</div>
                     <div className="cities-jumbotron-input-container">
                         <div className="cities-magnify"><i className="material-icons">search</i></div>
-                        <Geosuggest placeholder="Find by city"/>
+                        <Geosuggest placeholder="Find by city"
+                          onSuggestSelect={this.onSuggestSelectCity}
+                          />
                     </div>
-                    
+
                 </div>
             </div>
         );
--- src/services/citiesListService.js
+++ src/services/citiesListService.js
@@ -1,6 +1,11 @@
 import axios from 'axios';
 
 export function getCities() {
-  return axios.get('http://localhost:8000/cities')
+  return axios.get('/cities')
+  .then(res => res.data);
+}
+
+export function getZip(lat, lng) {
+  return axios.get('https://maps.googleapis.com/maps/api/geocode/json?latlng=' + lat.toString() + ',' + lng.toString())
   .then(res => res.data);
 }
