4e176ecba58d289095609464455e89638db10489
Nohema Call <you@example.com>
2016-10-31 13:51:34 -0600

    fix merge issue

---
+++ src/components/Rides/RidesComp/RidesComp.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './RidesComp.css';
+
+class Rides extends React.Component{
+    render(){
+        return (
+            <div>
+                This is the rides comp!
+            </div>
+        );
+    }
+};
+
+export default Rides;
