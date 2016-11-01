16868bb5c8cd0223c212156413d90bcb9f94379e
Kenna Martin <kennalmartin@gmail.com>
2016-11-01 15:57:00 -0600

    added request ride comp and altered cities table in schema

--- src/components/Rides/RidesComp/RidesComp.jsx
+++ src/components/Rides/RidesComp/RidesComp.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import RequestRide from '../RequestRide/RequestRide.jsx';
+
 import './RidesComp.css';
+
 
 class Rides extends React.Component{
     render(){
         return (
             <div>
                 This is the rides comp!
+                <RequestRide />
             </div>
         );
     }
