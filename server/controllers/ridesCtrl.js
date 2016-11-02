9fc088c08f56e04c3e4c450153463091ea49572a
Nohema Call <you@example.com>
2016-11-02 17:02:08 -0600

    fixing merge conflict

--- server/controllers/ridesCtrl.js
+++ server/controllers/ridesCtrl.js
@@ -1,42 +1,16 @@
 var app = require('../../server.js');
 var db = app.get('db');
-
-
 module.exports = {
-  createRide: function(req, res, next) {
-      db.createRide([req.body.originlat, req.body.originlong, req.body.origindate, req.body.destlat, req.body.destlong, req.body.driver_id, req.body.rider_id], function(err, ride) {
+
+    google: function(req, res, next) {
+      db.get_cities(function(err, cities) {
         if(err) {
           res.status(500).json(err);
         }
         else {
-          res.status(200).json('Your ride will arrive in 5 minutes.');
+          res.status(200).json(cities);
         }
-      })
-  },
-
-
-  arriveRide: function(req, res, next) {
-    db.arriveRide([req.body.rideid], function(err) {
-      if(err) {
-        res.status(500).json(err);
-      }
-      else {
-        res.status(200).json('Your driver has arrived.');
-      }
-    })
-  },
-
-
-  cancelRide: function(req, res, next) {
-    db.cancelRide([req.body.rideid], function(err){
-      if(err) {
-        res.status(500).json(err);
-      }
-      else {
-        res.status(200).json('Your ride has been cancelled.');
-      }
-    })
-  }
-
+      });
+    },
 
 }
