e1016393a05f679e152f2e88e977c969f296729c
Kenna Martin <kennalmartin@gmail.com>
2016-11-03 11:34:06 -0600

    added files needed to create ride status components

---
+++ src/components/Rides/Request/Request.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Request.css';
+
+class Request extends React.Component {
+  render() {
+    return(
+      <div></div>
+    )
+  }
+}
+
+export default Request;
--- src/components/Rides/RidesMap/MapComp.jsx
+++ src/components/Rides/RidesMap/MapComp.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SetPickup from '../SetPickup/SetPickup.jsx';
+import Request from '../Request/Request.jsx';
+import Cancel from '../Cancel/Cancel.jsx';
+import StartRide from '../StartRide/StartRide.jsx';
+import RateRide from '../RateRide/RateRide.jsx';
+import Payment from '../Payment/Payment.jsx';
+
 
 import './MapComp.css';
 import {getNearbyDrivers} from '../../../services/GetGoogleMap.js';
@@ -21,50 +28,13 @@ class MapComp extends React.Component {
     render() {
      return (<div className="map">
        <div className='map-page' ref="mapCanvas">
-       </div>
-
-                                         <div className='RequestRides'>
-
-                                             <div className='second'>
-
-                                                 <div className='fristBox'>
-                                                     <div className='box_for_google_text'>
-                                                     <div className='g'>G</div>
-                                                     <div className='o'>o</div>
-                                                     <div className='o2'>o</div>
-                                                     <div className='g'>g</div>
-                                                     <div className='l'>l</div>
-                                                     <div className='o'>e</div>
-                                                     </div>
-                                                     <div className='gps'></div>
-                                                 </div>
-
-                                                 <div className='secondBox'>
-                                                   <div className='car_icon'>
-                                                     Shyft
-                                                   </div>
-                                                          <div className='content'>
-                                                                   <div className='location'>
-                                                                      location
-                                                                   </div>
-                                                                   <div className='pickup_location'>
-                                                                      PickUp location
-                                                                   </div>
-                                                          </div>
-                                                 </div>
-
-                                                 <div className='secondBox2'>
-
-                                                   <div className='pickup_button'>
-                                                     Set PickUp
-                                                   </div>
-
-                                                 </div>
-
-                                             </div>
-
-
-                                       </div>
+</div>
+    <SetPickup />
+    // <Request />
+    // <Cancel />
+    // <StartRide/>
+    // <RateRide/>
+    // <Payment />
 
      </div>)
 
